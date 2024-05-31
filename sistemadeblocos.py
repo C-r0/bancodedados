00ed74ed6de63710aa5e2a6cdf4498769731d075
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-05-31 15:38:10 -0300

    Add files via upload

---
+++ sistemadeblocos.py
@@ -0,0 +1,13 @@
+print("Blocos com dados:")
+print("0, 1, 2")
+print("Escolha algum para ler")
+res = int(input())
+b1 = "x = 10"
+b2 = "Nome = Joao"
+b3 = "cor = azul"
+if res == 0:
+    print(b1)
+if res == 1:
+    print(b2)
+if res == 2:
+    print(b3)
